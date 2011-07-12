8e3c7cabff2fbb0e2b7bbf870928dc2c46ed2740
Jeff Brown <jeffbrown@google.com>
2011-07-11 22:11:59 -0700

    Remove the simulator target from all makefiles. Bug: 5010576
    Change-Id: I7731e32d21a2bbb2710bdde1515abac9b8e58309

--- Android.mk
+++ Android.mk
@@ -1,13 +1,5 @@
-ifneq ($(TARGET_SIMULATOR),true)
-  BUILD_IPTABLES_V14 := 1
-endif
-
-ifeq ($(BUILD_IPTABLES_V14),1)
+BUILD_IPTABLES_V14 := 1
 
 LOCAL_PATH:= $(call my-dir)
 
 include $(call all-subdir-makefiles)
-
-#----------------------------------------------------------------
-#----------------------------------------------------------------
-endif
