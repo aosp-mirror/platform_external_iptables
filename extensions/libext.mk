b4ad8a418b48b6a7df8f88a276c52f00c1bb43af
Colin Cross <ccross@android.com>
2014-01-27 16:30:26 -0800

    external/iptables: use local-generated-sources-dir
    local-intermediates-dir doesn't work for multiarch builds, because
    each architecture needs a separate intermediates dir.  Use
    local-generated-sources-dir, which gives a directory under $OUT/gen
    that can be shared by both architectures.  Files installed into
    $OUT/gen/*/*_intermediates and listed in LOCAL_GENERATED_SOURCES
    will be copied into $OUT/obj*/*/*_intermediates automatically as
    necessary.
    
    Change-Id: I78e7898147a0e2303e814e8b93f7cd0edbd2914e

--- extensions/libext.mk
+++ extensions/libext.mk
@@ -3,15 +3,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS:=
 LOCAL_MODULE:=libext$(libext_suffix)
 
-# LOCAL_MODULE_CLASS must be defined before calling $(local-intermediates-dir)
+# LOCAL_MODULE_CLASS must be defined before calling $(local-generated-sources-dir)
 #
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-MY_intermediates := $(call local-intermediates-dir)
+MY_gen := $(call local-generated-sources-dir)
 
 # LOCAL_PATH needed because of dirty #include "blabla.c"
 LOCAL_C_INCLUDES:= \
 	$(LOCAL_PATH)/../include/ \
-	$(MY_intermediates) \
+	$(MY_gen) \
 	$(LOCAL_PATH)
 
 LOCAL_CFLAGS:=-DNO_SHARED_LIBS=1
@@ -22,7 +22,7 @@ LOCAL_CFLAGS+=-DXTABLES_INTERNAL
 LOCAL_CFLAGS+=-D__ANDROID__
 LOCAL_CFLAGS += $(MY_warnings)
 
-MY_GEN_INITEXT:= $(MY_intermediates)/initext.c
+MY_GEN_INITEXT:= $(MY_gen)/initext.c
 $(MY_GEN_INITEXT): MY_initext_func := $(addprefix $(libext_prefix)_,$(libext_build_mod))
 $(MY_GEN_INITEXT): MY_suffix := $(libext_suffix)
 $(MY_GEN_INITEXT):
@@ -44,16 +44,16 @@ $(MY_GEN_INITEXT):
 MY_lib_sources:= \
 	$(patsubst %,$(LOCAL_PATH)/lib$(libext_prefix)_%.c,$(libext_build_mod))
 
-MY_gen_lib_sources:= $(patsubst $(LOCAL_PATH)/%,${MY_intermediates}/%,${MY_lib_sources})
+MY_gen_lib_sources:= $(patsubst $(LOCAL_PATH)/%,${MY_gen}/%,${MY_lib_sources})
 
 ${MY_gen_lib_sources}: PRIVATE_PATH := $(LOCAL_PATH)
 ${MY_gen_lib_sources}: PRIVATE_CUSTOM_TOOL = $(PRIVATE_PATH)/filter_init $(PRIVATE_PATH)/$(notdir $@) > $@
 ${MY_gen_lib_sources}: PRIVATE_MODULE := $(LOCAL_MODULE)
 ${MY_gen_lib_sources}: PRIVATE_C_INCLUDES := $(LOCAL_C_INCLUDES)
-${MY_gen_lib_sources}: $(MY_lib_sources)
+${MY_gen_lib_sources}: ${MY_gen}/% : $(LOCAL_PATH)/%
 	$(transform-generated-source)
 
-$(MY_intermediates)/initext$(libext_suffix).o : $(MY_GEN_INITEXT) $(MY_gen_lib_sources)
+$(MY_GEN_INITEXT): $(MY_gen_lib_sources)
 
 LOCAL_GENERATED_SOURCES:= $(MY_GEN_INITEXT) $(MY_gen_lib_sources)
 
