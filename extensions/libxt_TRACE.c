4bdf0ae602c4f53b05b8fe903981a92f8f2a65b0
Liping Zhang <liping.zhang@spreadtrum.com>
2016-06-08 20:47:03 +0800

    extensions: libxt_TRACE: Add translation to nft
    For example:
    
      # iptables-translate -t raw -A PREROUTING -j TRACE
      nft add rule ip raw PREROUTING counter nftrace set 1
    
    Signed-off-by: Liping Zhang <liping.zhang@spreadtrum.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- extensions/libxt_TRACE.c
+++ extensions/libxt_TRACE.c
@@ -7,12 +7,20 @@
 #include <xtables.h>
 #include <linux/netfilter/x_tables.h>
 
+static int trace_xlate(const void *ip, const struct xt_entry_target *target,
+		       struct xt_xlate *xl, int numeric)
+{
+	xt_xlate_add(xl, "nftrace set 1");
+	return 1;
+}
+
 static struct xtables_target trace_target = {
 	.family		= NFPROTO_UNSPEC,
 	.name		= "TRACE",
 	.version	= XTABLES_VERSION,
 	.size		= XT_ALIGN(0),
 	.userspacesize	= XT_ALIGN(0),
+	.xlate		= trace_xlate,
 };
 
 void _init(void)
