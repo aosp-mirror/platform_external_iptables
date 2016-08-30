aosp-mirror/platform_external_iptables

commit 7a0992da44cfb6cab0ccd1beadcf326df8773552

src: introduce struct xt_xlate_{mt,tg}_params
This structure is an extensible containers of parameters, so we don't
need to propagate interface updates in every extension file in case
we need to add new parameters in the future.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

extensions/libipt_DNAT.c
@@ -262,10 +262,10 @@ static void print_range_xlate(const struct nf_nat_ipv4_range *r,
 	}
 }
 
-static int DNAT_xlate(const void *ip, const struct xt_entry_target *target,
-		      struct xt_xlate *xl, int numeric)
+static int DNAT_xlate(struct xt_xlate *xl,
+		      const struct xt_xlate_tg_params *params)
 {
-	const struct ipt_natinfo *info = (const void *)target;
+	const struct ipt_natinfo *info = (const void *)params->target->data;
 	unsigned int i = 0;
 	bool sep_need = false;
 	const char *sep = " ";

extensions/libipt_SNAT.c
@@ -273,10 +273,10 @@ static void print_range_xlate(const struct nf_nat_ipv4_range *r,
 	}
 }
 
-static int SNAT_xlate(const void *ip, const struct xt_entry_target *target,
-		      struct xt_xlate *xl, int numeric)
+static int SNAT_xlate(struct xt_xlate *xl,
+		      const struct xt_xlate_tg_params *params)
 {
-	const struct ipt_natinfo *info = (const void *)target;
+	const struct ipt_natinfo *info = (const void *)params->target->data;
 	unsigned int i = 0;
 	bool sep_need = false;
 	const char *sep = " ";

configure.ac
@@ -2,7 +2,7 @@
 AC_INIT([iptables], [1.6.0])
 
 # See libtool.info "Libtool's versioning system"
-libxtables_vcurrent=11
+libxtables_vcurrent=12
 libxtables_vage=0
 
 AC_CONFIG_AUX_DIR([build-aux])

extensions/libip6t_DNAT.c
@@ -252,10 +252,10 @@ static void print_range_xlate(const struct nf_nat_range *range,
 	}
 }
 
-static int DNAT_xlate(const void *ip, const struct xt_entry_target *target,
-		      struct xt_xlate *xl, int numeric)
+static int DNAT_xlate(struct xt_xlate *xl,
+		      const struct xt_xlate_tg_params *params)
 {
-	const struct nf_nat_range *range = (const void *)target->data;
+	const struct nf_nat_range *range = (const void *)params->target->data;
 	bool sep_need = false;
 	const char *sep = " ";
 

extensions/libip6t_LOG.c
@@ -182,12 +182,12 @@ static const struct ip6t_log_xlate ip6t_log_xlate_names[] = {
 	{"warn",        LOG_WARNING }
 };
 
-static int LOG_xlate(const void *ip, const struct xt_entry_target *target,
-		     struct xt_xlate *xl, int numeric)
+static int LOG_xlate(struct xt_xlate *xl,
+		     const struct xt_xlate_tg_params *params)
 {
-	unsigned int i = 0;
 	const struct ip6t_log_info *loginfo =
-			(const struct ip6t_log_info *)target->data;
+		(const struct ip6t_log_info *)params->target->data;
+	unsigned int i = 0;
 
 	xt_xlate_add(xl, "log ");
 	if (strcmp(loginfo->prefix, "") != 0)

extensions/libip6t_MASQUERADE.c
@@ -131,11 +131,10 @@ MASQUERADE_save(const void *ip, const struct xt_entry_target *target)
 		printf(" --random");
 }
 
-static int
-MASQUERADE_xlate(const void *ip, const struct xt_entry_target *target,
-		 struct xt_xlate *xl, int numeric)
+static int MASQUERADE_xlate(struct xt_xlate *xl,
+			    const struct xt_xlate_tg_params *params)
 {
-	const struct nf_nat_range *r = (const void *)target->data;
+	const struct nf_nat_range *r = (const void *)params->target->data;
 
 	xt_xlate_add(xl, "masquerade");
 

extensions/libip6t_REDIRECT.c
@@ -132,10 +132,10 @@ static void REDIRECT_save(const void *ip, const struct xt_entry_target *target)
 	}
 }
 
-static int REDIRECT_xlate(const void *ip, const struct xt_entry_target *target,
-			  struct xt_xlate *xl, int numeric)
+static int REDIRECT_xlate(struct xt_xlate *xl,
+			  const struct xt_xlate_tg_params *params)
 {
-	const struct nf_nat_range *range = (const void *)target->data;
+	const struct nf_nat_range *range = (const void *)params->target->data;
 
 	if (range->flags & NF_NAT_RANGE_PROTO_SPECIFIED) {
 		xt_xlate_add(xl, "redirect to %hu",

extensions/libip6t_REJECT.c
@@ -139,11 +139,11 @@ static const struct reject_names_xlate reject_table_xlate[] = {
 	{"reject-route",	IP6T_ICMP6_REJECT_ROUTE}
 };
 
-static int REJECT_xlate(const void *ip, const struct xt_entry_target *target,
-			struct xt_xlate *xl, int numeric)
+static int REJECT_xlate(struct xt_xlate *xl,
+			const struct xt_xlate_tg_params *params)
 {
 	const struct ip6t_reject_info *reject =
-				(const struct ip6t_reject_info *)target->data;
+		(const struct ip6t_reject_info *)params->target->data;
 	unsigned int i;
 
 	for (i = 0; i < ARRAY_SIZE(reject_table_xlate); ++i)

commit 1d3f29d61b24ae1a41ef9acd3fcbe658a8892b9c

extensions: libipt_DNAT/SNAT: fix "OOM" when do translation to nft
When I want to translate SNAT target to nft rule, an error message
was printed out:
  # iptables-translate -A POSTROUTING -j SNAT --to-source 1.1.1.1
  iptables-translate v1.6.0: OOM

Because ipt_natinfo{} started with a xt_entry_target{}, so when we
get the ipt_natinfo pointer, we should use the target itself,
not its data pointer. Yes, it is a little tricky and it's different
with other targets.

Fixes: 7a0992da44cf ("src: introduce struct xt_xlate_{mt,tg}_params")
Signed-off-by: Liping Zhang <liping.zhang@spreadtrum.com>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

extensions/libipt_DNAT.c
@@ -265,7 +265,7 @@ static void print_range_xlate(const struct nf_nat_ipv4_range *r,
 static int DNAT_xlate(struct xt_xlate *xl,
 		      const struct xt_xlate_tg_params *params)
 {
-	const struct ipt_natinfo *info = (const void *)params->target->data;
+	const struct ipt_natinfo *info = (const void *)params->target;
 	unsigned int i = 0;
 	bool sep_need = false;
 	const char *sep = " ";

extensions/libipt_SNAT.c
@@ -276,7 +276,7 @@ static void print_range_xlate(const struct nf_nat_ipv4_range *r,
 static int SNAT_xlate(struct xt_xlate *xl,
 		      const struct xt_xlate_tg_params *params)
 {
-	const struct ipt_natinfo *info = (const void *)params->target->data;
+	const struct ipt_natinfo *info = (const void *)params->target;
 	unsigned int i = 0;
 	bool sep_need = false;
 	const char *sep = " ";